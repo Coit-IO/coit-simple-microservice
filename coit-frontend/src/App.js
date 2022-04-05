3d2f798909998c8a261ee878a68b02960eb647be
basil <basil1987@gmail.com>
2022-04-05 07:05:56 +0530

    commit latest changes

--- coit-frontend/src/App.js
+++ coit-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://127.0.0.1:4504/sentiment', {
+        fetch('http://13.127.201.84:8080/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
