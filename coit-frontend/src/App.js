797b99536b8d35dcc8f0b7ffaaebe8a6dde3b672
SrashtiD <srashtid8@gmail.com>
2022-04-14 08:34:06 +0530

    changed app.js file

--- coit-frontend/src/App.js
+++ coit-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://65.0.5.229:8080/sentiment', {
+        fetch('http://127.0.0.1:8080/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
