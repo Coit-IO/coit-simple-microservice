1a78d26a5f3f1d461f94baa62ce4428ca92a7c39
suma <sumasrivalle@gmail.com>
2022-10-05 18:35:17 +0530

    dsafg

--- coit-frontend/src/App.js
+++ coit-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://34.71.85.13:8080/sentiment', {
+        fetch('http://34.173.72.207:8080/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
